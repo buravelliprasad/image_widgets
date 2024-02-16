416fc8a50413de95eceb1562bd43e8d09680a5a5
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2024-02-16 19:32:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -646,7 +646,7 @@ def convert_markdown_links_to_html_images(text):
 
     # Replace all occurrences of markdown image links with HTML img tags
     html_text = re.sub(pattern, replace_with_img, text)
-    return html_text
+   return f'<a href="{url}"><img src="{url}" alt="{alt_text}"/></a>'
     
 output = ""
 with container:
